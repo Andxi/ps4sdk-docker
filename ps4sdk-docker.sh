262f438a6b4bfa6db3bad8c53febb10b2a3560d5
Pascal <pq@paskl.de>
2018-12-30 02:02:18 +0100

    map to host, so every port is forwarded to docker container

--- ps4sdk-docker.sh
+++ ps4sdk-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -z "$*" ]; then
-  docker run -v `pwd`:/build --rm -it ps4sdk-docker $*
+  docker run -v `pwd`:/build --rm -it --net host ps4sdk-docker $*
 else
-  docker run -v `pwd`:/build --rm -it -p 5350:5350 ps4sdk-docker
+  docker run -v `pwd`:/build --rm -it --net host ps4sdk-docker
 fi
