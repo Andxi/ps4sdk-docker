8eeca320a438f3a0a4380ba26015ea3c4ff8efda
Andxi <56528202+Andxi@users.noreply.github.com>
2020-03-16 21:20:12 -0700

    Update ps4sdk-docker.sh

--- ps4sdk-docker.sh
+++ ps4sdk-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ ! -z "$*" ]; then
-  docker run -v `pwd`:/build --rm -it --net host ps4sdk-docker $*
+  docker run -v `pwd`:/build --rm -it --192.168.1.83 ps4sdk-docker $*
 else
-  docker run -v `pwd`:/build --rm -it --net host ps4sdk-docker
+  docker run -v `pwd`:/build --rm -it --192.168.1.83 ps4sdk-docker
 fi
